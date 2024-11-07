0a977820062ee1161bb1873b71f3de0bd15ccadb
januszjgal <januszgal@pm.me>
2024-11-07 19:40:04 +0000

    Remote host variables added for Janusz env

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+# run.py
 from flask_backend.create_app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
     if app.config["FLASK_ENV"] == "development":
-        app.run(debug=True, port=5000)
+        app.run(debug=True, 
+                host='0.0.0.0' if os.environ.get('REMOTE') else 'localhost',
+                port=5000)
     else:
         app.run(debug=False)
--- vue-frontend/vite.config.js
+++ vue-frontend/vite.config.js
@@ -1,22 +1,15 @@
-// Plugins
+// vite.config.js
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
-
-// Utilities
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    vuetify({
-      autoImport: true,
-    }),
+    vue({ template: { transformAssetUrls } }),
+    vuetify({ autoImport: true }),
     ViteFonts({
       google: {
         families: [{
@@ -31,33 +24,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
   server: {
-    port: 3000, // Your desired port for the Vue app
+    host: process.env.REMOTE ? '0.0.0.0' : 'localhost',  // Use REMOTE env var to determine host
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // The URL of your Flask server
+        target: 'http://localhost:5000',
         changeOrigin: true,
       }
     },
-    sourcemap: false, // Add this to disable source maps in the dev server
+    sourcemap: false,
   },
-  // Configuration options for the build process
   build: {
-    // `sourcemap: false` disables source map generation
-    // Source maps help with debugging by mapping compiled code back to original source code,
-    // but they can cause errors if the source maps for certain libraries are missing.
-    // Disabling source maps prevents these errors in the browser console and is usually
-    // recommended in production to reduce build size and avoid unnecessary warnings.
     sourcemap: false,
   },
 })
